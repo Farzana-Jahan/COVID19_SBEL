cd5bd4dd5202093c07ca704de5e519120b0c3dcb
Farzana-Jahan <f.jahan@hdr.qut.edu.au>
2021-02-04 15:33:54 +1000

    adding shp files

--- R Scripts/Data preparation.R
+++ R Scripts/Data preparation.R
@@ -1,5 +1,15 @@
 library(tidyverse)
 # read the data from local directory
 data<- read_csv("data/owid-covid-data.csv")
+data_Europe<-filter(data,data$continent=="Europe")
+dim(data_Europe)
+names(data_Europe)
+
+summary(data_Europe$date)
+data_Europe<-data_Europe%>%
+  dplyr::mutate(year = lubridate::year(date),
+                month = lubridate::month(date),
+                day = lubridate::day(date))
+head(data_Europe)
 
 # looking at first 6 rows o f the data
